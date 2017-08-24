39926d5231200f42297b1b230742de3a88db877b
rkkmkkfx <baranovs-v@mail.ru>
2017-08-24 13:35:40 +0800

    Done!

--- dom/shop/js/index.js
+++ dom/shop/js/index.js
@@ -1,12 +1,14 @@
 const buttons = document.querySelectorAll('.add'),
       totalItems = document.querySelector('#cart-count'),
       totalPrice = document.querySelector('#cart-total-price');
+let currentItems = 0,
+  currentPrice = 0;
 
 function addToCart(event) {
-  let currentItems = parseInt(totalItems.innerText),
-      currentPrice = parseInt(totalPrice.innerText);
-  totalItems.innerText = currentItems + 1;
-  totalPrice.innerText = currentPrice + parseInt(event.target.dataset.price);
+  currentItems += 1;
+  currentPrice = currentPrice + parseInt(event.target.dataset.price);
+  totalItems.innerText = currentItems;
+  totalPrice.innerText = getPriceFormatted(currentPrice);
 }
 
 for (const button of buttons) {
