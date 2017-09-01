f2aa761ab93f93fb155d7a3187fe7de8ce4887c1
rkkmkkfx <baranovs-v@mail.ru>
2017-09-02 00:27:02 +0800

    Done!

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,7 @@
 'use strict';
+
+list.addEventListener('click', event => {
+  event.preventDefault();
+  const btn = event.target;
+  btn.classList.contains('add-to-cart') ? addToCart({title: btn.dataset.title, price: btn.dataset.price}) : false;
+});
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  const th = event.target,
+        table = event.currentTarget;
+
+  let ths = document.querySelectorAll('th');
+  ths = Array.from(ths).filter(item => {return item !== th});
+
+  Array.from(ths).forEach(item => {
+    item.removeAttribute('data-dir');
+  });
+
+  th.dataset.dir = th.dataset.dir ? -th.dataset.dir : 1;
+  sortTable(th.dataset.propName, th.dataset.dir);
+
+  table.dataset.sortBy = th.dataset.propName;
 }
